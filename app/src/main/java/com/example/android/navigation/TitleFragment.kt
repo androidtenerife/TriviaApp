dbaf009c9ac67ce447b4924ecbf55ad0967586bf
Herbert Daniel Rodríguez <tenerife.developer@gmail.com>
2021-03-20 21:05:50 +0000

    Add the NavHostFragment to hosted the diferent fragments.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,17 +1,22 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
 
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title2, container, false)
+        // Addeded the Binding Fragment to the ViewGroup
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        return binding.root
+        // return inflater.inflate(R.layout.fragment_title2, container, false)
     }
 
 }
