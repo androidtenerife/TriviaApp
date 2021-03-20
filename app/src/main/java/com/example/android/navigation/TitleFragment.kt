c33ec7ed0822b652f4f8d3e1c99a4b4d435117e7
Herbert Daniel Rodríguez <tenerife.developer@gmail.com>
2021-03-20 21:20:50 +0000

    Apply the androidx Jetpack optimization code

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -18,8 +18,8 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
 
         binding.playButton.setOnClickListener { view: View ->
-            Navigation.findNavController(view)
 
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
         return binding.root
     }
