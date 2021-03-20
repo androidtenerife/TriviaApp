87fc1c755483ba0539ded44f69e9171d50e78012
Herbert Daniel Rodríguez <tenerife.developer@gmail.com>
2021-03-20 21:14:28 +0000

    Add the Navigation Controller

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -15,8 +16,12 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         // Addeded the Binding Fragment to the ViewGroup
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener { view: View ->
+            Navigation.findNavController(view)
+
+        }
         return binding.root
-        // return inflater.inflate(R.layout.fragment_title2, container, false)
     }
 
 }
